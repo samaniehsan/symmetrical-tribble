c84f9281e283da1a36f9d739ec6ac0fa51597336
Ryan Rauch <ryan.rauch@outlook.com>
2018-11-07 01:16:08 -0600

    seek behavior updated to account for gravity acceleration

--- Symmetrical Tribble/Assets/_Completed-Game/Scripts/SeekBehavior.cs
+++ Symmetrical Tribble/Assets/_Completed-Game/Scripts/SeekBehavior.cs
@@ -5,32 +5,40 @@ public class SeekBehavior : MonoBehaviour
 {
     public GameObject target;   // target is set in the IDE
     public float speed;         // so is speed value
+    public float initialHeight;
+
+    private float _acceleration;
 
     // Use this for initialization
     void Start()
     {
-
+        _acceleration = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (target == null 
-            || !gameObject.activeSelf) 
+        if (target == null || !gameObject.activeSelf) 
         {
             return;
         }
 
-        // this behavior is closer to the "arrive" behavior in the book,
-        // and still needs to add in acceleration of gravity, as it slows down when close to target
-        Vector3 direction = Vector3.Normalize(target.transform.position - transform.position);
-        transform.position += direction * speed * Time.deltaTime;
+        float step = speed * Time.deltaTime;
+        _acceleration += step;
+        Vector2 xz = Vector2.MoveTowards(new Vector2(this.transform.position.x, this.transform.position.z),
+                                         new Vector2(target.transform.position.x, target.transform.position.z),
+                                         step);
+        this.transform.position = new Vector3(xz.x, 
+                                              initialHeight - _acceleration,
+                                              xz.y);
+
+        //Vector3 direction = Vector3.Normalize(target.transform.position - transform.position);
+        //transform.position += direction * speed * Time.deltaTime;
+
         // if bomb has reached the ground, remove it from view
-        if(transform.position.y < 0.5f)
+        if (transform.position.y < 0f)
         {
             gameObject.SetActive(false);
         }
-        //TODO: Add obstacle avoidance
-        // Pages 99-103 in the book describe this
     }
 }
